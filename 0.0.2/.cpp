5f380ba5a3057d7f2e51bb65a2969e8545736bfb
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-11-11 16:35:17 +0300

    Update .cpp

--- 0.0.2/.cpp
+++ 0.0.2/.cpp
@@ -12,36 +12,36 @@ int main() {
 	string result;
 	string count;
 	string first, second;
-	int length_int;
+	int length;
 	int counter = 0;
-	int k = 0;
-	int z = 0;
+	int c = 0;
+	int d = 0;
 	cout << "Введите размер последовательности: ";
 	getline(cin, a);
 	cout << "Введите элементы последовательности, согласно ее размеру: ";
 	getline(cin, b);
-	length_int = atoi(a.c_str());
-	for (int i = 0; i < b.length(); i++)	
+	length = atoi(a.c_str());
+	for (int i = 0; i < b.length(); i++)
 	{
 		if (b[i] == ' ') counter++;
 	}
-	if (length_int != (counter + 1))
+	if (length != (counter + 1))
 		cout << "При чтении была обнаружена ошибка!" << endl;
-	else 
+	else
 	{
 		cout << "Введите количество элементов, на которое необходимо сдвинуть последовательнось: ";
 		getline(cin, count);
-		if (count == "") 
+		if (count == "")
 		{
 			cout << "При чтении была обнаружена ошибка!" << endl;
 		}
 		else
 		{
-			while (k != (length_int - atoi(count.c_str())))
+			while (c != (length - atoi(count.c_str())))
 			{
-				second = second + b[z];
-				if (b[z] == ' ') k++;
-				z++;
+				second = second + b[d];
+				if (b[d] == ' ') c++;
+				d++;
 			}
 			for (int i = second.length(); i < b.length(); i++)
 			{
