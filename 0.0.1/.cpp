ed568cc72a6466382062a9f46830a41673fb6de5
BookaZluka <32384558+BookaZluka@users.noreply.github.com>
2017-12-21 01:03:12 +0300

    Update .cpp

--- 0.0.1/.cpp
+++ 0.0.1/.cpp
@@ -1,54 +1,24 @@
 #include <iostream>
-#include <string>
 
 int main() {
-
-	using namespace std;
-
-	setlocale(LC_ALL, "Russian");
-	string a;
-	string b;
-	string integer_str;
-	string result;
-	int length;
-	int counter = 0;
-	int с = 0;
-	cout << "Введите количество элементов последовательности: ";
-	getline(cin, a);
-	cout << "Введите последовательность для реверса, согласно ее разеру: ";
-	getline(cin, b);
-	length = atoi(a.c_str());
-	for (int i = 0; i < b.length(); i++)
-	{
-		if (b[i] == ' ') counter++;
-	}
-	if (length != (counter + 1))
-		cout << "При чтении была обнаружена ошибка!" << endl;
-	else
-	{
-		for (int i = 0; i < (b.length() / 2); i++)
-		{
-			swap(b[i], b[(b.length()) - i - 1]);
-		}
-		while (true)
-		{
-			for (int i = с; i < b.length(); i++) {
-				if (b[i] == ' ') {
-					с++;
-					break;
-				}
-				integer_str = integer_str + b[i];
-				с++;
-			}
-			if (integer_str == "") break;
-			for (int j = 0; j < ((integer_str.length()) / 2); j++)
-			{
-				swap(integer_str[j], integer_str[integer_str.length() - j - 1]);
-			}
-			result = result + integer_str + " ";
-			integer_str = "";
-		}
-		cout << "Результат: " << result << endl;
-	}
-	cin.get();
+    using namespace std;
+    int n, m;
+    cout << "Введите размер массива" << endl;
+    cin >> n;
+    if ( n <= 0 ) {
+        cout << "error" << endl;
+        return 1;
+    }
+    cout << "Введите элементы массива" << endl;
+    int *a = new int [n];
+    for (m=0; m<n; m++){
+        cin >> a[m];
+    }
+    for (m=0; m<n/2; m++){
+        swap (a[m], a[n-m-1]);
+    }
+    for (m=0; m<n; m++){
+        cout << a[m] << "\t";
+    }
+    delete [] a;
 }
